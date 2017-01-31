a1e7707273c5b54b60b758efc8eabe4f4ac137d2
deepakexcel <deepakexcel2011@gmail.com>
2017-01-31 12:32:33 +0530

    Added price history chart in products

--- www/lib/angular-chartist.js
+++ www/lib/angular-chartist.js
@@ -1,7 +1,7 @@
 (function () {
     var angularChartist = angular.module('angular-chartist', []);
 
-    angularChartist.directive('chartist', function () {
+    angularChartist.directive('chartist', function ($timeout) {
         var linkFn = function (scope, elm, attrs) {
             var data, options, responsiveOptions, selector, updateChart, deepWatchData, deepWatchOptions, deepwatch;
             data = scope.data;
@@ -11,8 +11,38 @@
             selector = "#" + scope.ngId;
             type = scope.type || 'line';
             elm.attr('id', scope.ngId);
-
-            updateChart = function () { 
+            if (selector == '#price_history_chart') {
+                var chart_data = {
+                    labels: [],
+                    series: []
+                };
+                chart_data.series[0] = [];
+                for (var key in data) {
+                    chart_data.labels.push(data[key].date1);
+                    chart_data.series[0].push(data[key].price);
+                }
+                options = {
+                    // Don't draw the line chart points
+                    showPoint: true,
+                    // Disable line smoothing
+                    lineSmooth: false,
+                    // X-Axis specific configuration
+                    axisX: {
+                        // We can disable the grid for this axis
+                        showGrid: false,
+                        // and also don't show the label
+                        showLabel: true
+                    },
+                    // Y-Axis specific configuration
+                    axisY: {
+                    }
+                };
+                data = chart_data;
+                $timeout(function () {
+                    elm.css('height', (elm[0].clientHeight + 5) + 'px');
+                }, 100)
+            }
+            updateChart = function () {
                 if (type === 'line') {
                     Chartist.Line(selector, data, options, responsiveOptions);
                 } else if (type === 'bar') {
